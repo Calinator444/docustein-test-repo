f2884834e6a3ad535d52a9f0a0983fa7901d7219
Calinator444 <calebwilliams@ssw.com.au>
2025-12-09 09:22:03 +1100

    added expiry date interval check for aggregate-links method

--- .github/scripts/tina-helpers/src/utils/aggregate-links.ts
+++ .github/scripts/tina-helpers/src/utils/aggregate-links.ts
@@ -1,4 +1,5 @@
 import TinaClient from '../services/tina-client';
+import Dayjs from 'dayjs';
 
 import 'dotenv/config'
 
@@ -6,6 +7,7 @@ const aggregateContent = async (desiredLength : number)=> {
 
     const tinaClientId = process.env.TINA_CLIENT_ID;
     const tinaToken = process.env.TINA_TOKEN;
+    const expiryInterval = process.env.TINA_AUDITOR_EXPIRY_DAYS;
 
     if(!tinaClientId){
         console.error("Error: TINA_CLIENT_ID is not set in environment secrets.");
@@ -17,6 +19,10 @@ const aggregateContent = async (desiredLength : number)=> {
         process.exit(1);
     }
 
+    if(!expiryInterval) {
+        console.error("Error: TINA_AUDITOR_EXPIRY_DAYS is not set in environment variables.");
+    }
+
     const tinaClient = new TinaClient(tinaClientId, tinaToken);
     
     const allLinks: Array<string> = [];
@@ -25,14 +31,23 @@ const aggregateContent = async (desiredLength : number)=> {
         for(const key of Object.keys(content.data)) {
             const data = content.data[key]!;
             const edges = data.edges;
+            var day = Dayjs();
+
+            var expiryDate = day.subtract(Number(expiryInterval), 'day');
+
 
             for(const edge of edges) {
                 const node = edge.node;
                 const path = node._sys.path;
+                
                 if(edge.node.lastChecked){
                     continue;
                 }
-                allLinks.push(path);
+                
+                if((Dayjs(edge.node.lastChecked).isAfter(expiryDate))) {
+                    allLinks.push(path);
+                }
+
                 if(allLinks.length === desiredLength)
                 {
                     return allLinks;
